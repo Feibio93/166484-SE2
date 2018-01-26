64bf51b26425a710b375dcc748e4a9748639666d
Feibio93 <32126741+Feibio93@users.noreply.github.com>
2018-01-26 11:26:56 +0100

    Add files via upload

---
+++ checker.js
@@ -0,0 +1,34 @@
+
+const fetch = require('node-fetch')
+
+function check(url, invocationParameters,  expectedResultData, expectedResultStatus) {
+
+    const checkResult = { // this is the object you need to set and return
+        urlChecked: url,
+        resultData: null,
+        resultStatus: null,
+        statusTestPassed: null,
+        resultDataAsExpected: null
+    }
+    var call = XMLHttpRequest(invocationParameters);
+    call.open('GET', url, true);
+    
+    
+    
+    var answer = compareResults(checkResult,call);
+    if (answer) return checkResult    
+}
+
+
+// funzione che confronta due oggetti semplici e verifica se actual contiene tutti gli attributi di expected, e se per
+// questi ha gli stessi valori
+function compareResults(expected, actual) {
+    if (!expected) return true //always ok if there are no expectations
+    if (!actual) return false
+    for (let e of Object.keys(expected)) {
+        if (actual[e]===undefined || expected[e]!=actual[e]  ) return false
+    }
+    return true
+}
+
+module.exports = check
